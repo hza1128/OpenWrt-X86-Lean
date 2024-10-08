c970b152f7405918f93514eaa04061d6f8bc9f6d
hza1128 <141570215+hza1128@users.noreply.github.com>
2024-10-08 09:52:58 +0800

    Update custom.sh

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -249,6 +249,7 @@ CONFIG_PACKAGE_luci-app-passwall=y
 CONFIG_PACKAGE_luci-app-passwall2=n
 # CONFIG_PACKAGE_naiveproxy=y
 CONFIG_PACKAGE_chinadns-ng=y
+CONFIG_PACKAGE_INCLUDE_SingBox=y
 # CONFIG_PACKAGE_brook=y
 CONFIG_PACKAGE_trojan-go=y
 CONFIG_PACKAGE_xray-plugin=y
